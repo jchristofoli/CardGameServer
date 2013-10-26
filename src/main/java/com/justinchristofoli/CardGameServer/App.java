0c3a9ce67fb2df1f8aaae43d387536f699565fc9
Justin Christofoli <jchristofoli@gmail.com>
2013-10-25 21:58:18 -0400

    Simple Hello World servlet.

--- src/main/java/com/justinchristofoli/CardGameServer/App.java
+++ src/main/java/com/justinchristofoli/CardGameServer/App.java
@@ -12,6 +12,7 @@ public class App
     public static void main( String[] args )
     {
         Server server = new Server(8080);
+	    server.setHandler(new TestHandler());
         try {
 			server.start();
 			server.join();
